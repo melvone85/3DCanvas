c56cf9ebd1c0fa4f8e5eae1b9ee051b6c2d19024
marco <marcocontini85@gmail.com>
2022-11-09 16:55:14 +0100

    Cleaned

--- ParserLib/Models/ProgramContext.cs
+++ ParserLib/Models/ProgramContext.cs
@@ -6,13 +6,14 @@ namespace ParserLib.Models
 {
     public class ProgramContext : IProgramContext
     {
-        public IEntity ReferenceMove    { get; set; }
-        public IEntity LastEntity       { get;set; }
-        public bool IsIncremental       { get;set; }
-        public bool InMainProgram       { get;set; }
-        public bool IsBeamOn { get;set; }
-        public bool IsMarkingProgram { get;set;}
+        public IEntity ReferenceMove { get; set; }
+        public IEntity LastEntity { get; set; }
         public ELineType ContourLineType { get; set; }
+
+        public bool IsIncremental { get; set; }
+        public bool InMainProgram { get; set; }
+        public bool IsBeamOn { get; set; }
+        public bool IsMarkingProgram { get; set; }
         public int SourceLine { get; set; }
         public bool IsInchProgram { get; set; }
         public bool Is3DProgram { get; set; }
